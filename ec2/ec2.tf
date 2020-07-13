22bbf97a7979bb99b0716e4c026311a3321f4cc0
chandurajub <chandu.raju.abc@gmail.com>
2020-07-13 16:50:56 +0530

    message

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -28,18 +28,18 @@ resource "aws_instance" "ec2" {
   ami = "ami-0c84a3e93390c29bc"
   instance_type = "t2.micro"
   subnet_id = element(var.private_sub,0)
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 }
 resource "aws_instance" "ec21" {
   ami = "ami-0c84a3e93390c29bc"
   instance_type = "t2.micro"
   subnet_id = element(var.public_sub,2)
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 }
 resource "aws_instance" "ec22" {
   ami = "ami-0c84a3e93390c29bc"
   instance_type = "t2.micro"
   subnet_id = element(var.public_sub,0)
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 }
 
